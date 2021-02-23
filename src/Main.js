8cddd6457a8282205eea68a6a189f53d4ba8ad09
Benjamin-Mwiti <66462102+Benjamin-Mwiti@users.noreply.github.com>
2021-02-23 13:25:05 +0300

    Nice API

--- src/Main.js
+++ src/Main.js
@@ -11,7 +11,7 @@
 ******************************************************************************
 **/
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import './style.css';
 import $ from 'jquery';
 
@@ -19,11 +19,11 @@ function Main() {
   
   const [city_name, setCity_Name] = useState("");
   
-  const baseURL = `http://api.openweathermap.org/data/2.5/weather?q=${city_name}&units=metric&appid=1cc7ad57a30f3ba7be0d6a9766a69562`;
+  const openWeatherMapURL = `http://api.openweathermap.org/data/2.5/weather?q=${city_name}&units=metric&appid=1cc7ad57a30f3ba7be0d6a9766a69562`;
 
   $(function() {
     $.ajax({
-      url: baseURL,
+      url: openWeatherMapURL,
       type: "GET",
       /* 
       * JavaScript Object Nottion Pattern
@@ -31,8 +31,24 @@ function Main() {
       */
       dataType: "jsonp",
       success: function(data) {
-        let cityForecast_2 = $('.city__forecast > .city__stats').eq(1);
-        $(cityForecast_2).text(data.name + ", " + data.sys.country);
+        let cityForecast_2 = $('.city__forecast > .city__stats > p');
+        // Weather icon
+        $('.city__forecast > span').html("<img src=http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png />");
+
+        // First child of city__stats class
+        $(cityForecast_2.find('strong > span')).text(data.name + ", " + data.sys.country);
+        $(cityForecast_2.find('strong > i')).text(data.weather[0].description);
+
+        // Second child of city__stats class
+        $(cityForecast_2.find('.temp').eq(0)).text(data.main.temp/*  + <span><sup>o</sup>C</span> */);
+        $(cityForecast_2.find('.temp').eq(1)).text(data.main.temp_min);
+        $(cityForecast_2.find('.temp').eq(2)).text(data.main.temp_max);
+        $(cityForecast_2.find('.wind__speed')).text(data.wind.speed);
+        $(cityForecast_2.find('.clouds')).text(data.clouds.all);
+
+        // Third child of city__stats class
+        $(cityForecast_2.eq(2).find('span')).text("[" + data.coord.lon + ", " + data.coord.lat + "]");
+        $(cityForecast_2.eq(1).find('.pressure')).text(data.main.pressure);
       }
     });
   });
@@ -49,28 +65,26 @@ function Main() {
               onChange = {e => {
                 setCity_Name(e.target.value);
               }} />
-            <button type="submit" onClick={(e) => { 
-              console.log(city_name);
-              setCity_Name("");
+            <button type="submit" onClick={(e) => {
               e.preventDefault();
             }} >Search</button>
           </form>
         </div>
       </div>
       <div className="app__body">
-        <div className="city__forecast">
-          <span>icon</span>
+        {/* <div className="city__forecast">
+          <span>Icon</span>
           <div className="city__stats">
             <p><strong><span>Toronto, CA</span> <i>broken clouds</i></strong></p>
             <p><span>-0.7<sup>o</sup>C</span> temperature from -1.1 to 0<sup>o</sup>C, wind 4.52m/s. clouds 77 %, 1008 hpa</p>
             <p>Geo coords <span>[43.7001, -79.4163]</span></p>
           </div>
-        </div>
+        </div> */}
         <div className="city__forecast">
-          <span>icon</span>
+          <span></span>
           <div className="city__stats">
             <p><strong><span>Toronto, US</span> <i>overcast clouds</i></strong></p>
-            <p><span>0.9<sup>o</sup>C</span> temperature from 0.6 to 1.1<sup>o</sup>C, wind 3.13m/s. clouds 100 %, 1013 hpa</p>
+            <p><span><span className="temp average__temp">0.9</span><sup>o</sup>C</span> temperature from <span className="temp">0.6</span> to <span className="temp">1.1</span><sup>o</sup>C, wind <span className="wind__speed">3.13</span>m/s. clouds <span className="clouds">100</span> %, <span className="pressure">1013</span> hpa</p>
             <p>Geo coords <span>[40.4642, -80.6009]</span></p>
           </div>
         </div>
